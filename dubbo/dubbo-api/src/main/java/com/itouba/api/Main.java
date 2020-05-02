69b7945358007894bce6eecf03f076b6356c24f5
itouba <11712880@qq.com>
2020-05-02 21:14:01 +0800

    add

---
+++ dubbo/dubbo-api/src/main/java/com/itouba/api/Main.java
@@ -0,0 +1,12 @@
+package com.itouba.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    //你的家伙是谁
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ dubbo/dubbo-api/src/main/java/com/itouba/api/iservice/IUser.java
@@ -0,0 +1,8 @@
+package com.itouba.api.iservice;
+
+import com.itouba.api.entity.User;
+
+public interface IUser {
+
+    public User getUser();
+}
